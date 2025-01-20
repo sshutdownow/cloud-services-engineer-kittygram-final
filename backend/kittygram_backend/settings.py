2f5e86c41488610f0bb2468389c2b3cc837f4922
Igor Popov <ip-adsense@yandex.ru>
2025-01-21 00:00:24 +0200

    almost all

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -57,8 +57,13 @@ WSGI_APPLICATION = 'kittygram_backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+#        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': os.getenv("POSTGRES_DB", "postgres"), # Returns `postgres` if the key doesn't exist
+        'USER': os.getenv("POSTGRES_USER", "postgres"),
+        'PASSWORD': os.getenv("POSTGRES_PASSWORD", "postgres"),
+        'HOST': os.getenv("POSTGRES_HOST", "localhost"),
+        'PORT': os.getenv("POSTGRES_PORT", 5432)
     }
 }
 
