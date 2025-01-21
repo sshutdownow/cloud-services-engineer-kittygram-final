717f76e6c427e7d4ff9da29adac13af80d6972fa
Igor Popov <ip-adsense@yandex.ru>
2025-01-22 00:55:42 +0200

    settings.py ALLOWED_HOSTS = ['*']

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -10,7 +10,8 @@ SECRET_KEY = os.environ.get("DJUNGO_SECRET_KEY", get_random_secret_key())
 
 DEBUG = bool(os.getenv("DJUNGO_DEBUG", ''))
 
-ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS", 'localhost 127.0.0.1 [::1]').split(' ')
+ALLOWED_HOSTS = ['*']
+# ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS", 'localhost 127.0.0.1 [::1]').split(' ')
 
 INSTALLED_APPS = [
     'django.contrib.admin',
