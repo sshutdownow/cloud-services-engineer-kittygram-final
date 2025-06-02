3cfa0c625b35409d401b62c1f027ec8f0be7c1a4
Igor Popov <sshutdownow@users.noreply.github.com>
2025-06-02 12:54:18 +0200

    Update vm.tf

--- infra/vm.tf
+++ infra/vm.tf
@@ -35,6 +35,7 @@ resource "yandex_compute_instance" "vm_1" {
     serial-port-enable = "1"
     user-data = templatefile("${path.module}/init/vm-install.yml", 
     {
+      USER = var.vm_user
       SSH_KEY = var.ssh_key
     })
   }
